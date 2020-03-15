e296913e039f25b9d0cf846bc713c0360e25be83
yuu-eguci <yuu.eguci@gmail.com>
2020-03-15 18:22:49 +0900

    :hammer: Update gist with real data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import json
 import requests
 import dotenv
-from pprint import pprint
+import math
 
 
 def get_my_logger():
@@ -68,39 +68,74 @@ def get_env(keyname: str) -> str:
         raise EnvNotFoundError(keyname) from e
 
 
+def _generate_bar_chart(percent: float, size: int) -> str:
+    """バーチャートを生成します。
+    ↓こういうやつです。
+    ████████████▍░░
+
+    Arguments:
+        percent {float} -- パーセンテージ値。
+        size {int} -- バーのマス数。
+
+    Returns:
+        str -- バーチャート。
+    """
+    full = '█'
+    semifull = '█▏▎▍▌▋▊▉'
+    empty = '░'
+    frac = math.ceil(size * 8 * percent / 100)
+    barsFull = math.ceil(frac / 8)
+    semi = int(frac % 8)
+    barsEmpty = size - barsFull
+    return ''.join([
+        full * (barsFull - 1),
+        semifull[semi],
+        empty * barsEmpty,
+    ])
+
+
+def generate_file_content_line(raw_data: dict) -> str:
+    """fileメインファイルコンテンツの1行を生成します。
+    ↓こういうやつです。
+    Python      2 hrs 36 mins  ██████▍░░░░░░░░  41.9%
+
+    Arguments:
+        raw_data {dict} -- name, text, percent をもつディクショナリ。
+
+    Returns:
+        str -- コンテンツの1行。
+    """
+    return ' '.join([
+        # name が言語名。
+        raw_data['name'].ljust(11),
+        # text が期間。
+        raw_data['text'].ljust(14),
+        # バーチャート。
+        _generate_bar_chart(raw_data['percent'], 15),
+        # パーセンテージ。
+        str(round(raw_data['percent'], 1)).rjust(5) + '%',
+    ])
+
+
 # .env で環境変数を取得する場合に対応します。見つからなくてもエラーを起こさない。
 dotenv.load_dotenv(dotenv.find_dotenv(raise_error_if_not_found=False))
 wakatime_secret_api_key = get_env('WAKATIME_SECRET_API_KEY')
 
 logger = get_my_logger()
-logger.info('foobarbaz')
+logger.info('処理開始。')
 
 response = requests.get(
     # NOTE: 改行は逆に見づらいので E501 を無視します。
     f'https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key={wakatime_secret_api_key}')  # noqa: E501
 response_json = json.loads(response.text)
-pprint(response_json['data']['languages'])
+logger.info('WakaTime stats 取得完了。')
 
-# 仮のファイルコンテンツ。
-temporary_file_content = '\n'.join([
-    'YAML        9 hrs 22 mins  ██████░░░░░░░░░░░░░░  31.1%',
-    'TypeScript  6 hrs 50 mins  ████░░░░░░░░░░░░░░░░  22.7%',
-    'JSON        5 hrs 42 mins  ███░░░░░░░░░░░░░░░░░  18.9%',
-    'Markdown    3 hrs 12 mins  ██░░░░░░░░░░░░░░░░░░  10.7%',
-    'Other       2 hrs 4 mins   █░░░░░░░░░░░░░░░░░░░   6.9%',
-    'JavaScript  58 mins        ░░░░░░░░░░░░░░░░░░░░   3.3%',
-    'Docker      56 mins        ░░░░░░░░░░░░░░░░░░░░   3.1%',
-    'Git Config  22 mins        ░░░░░░░░░░░░░░░░░░░░   1.2%',
-    'Go          13 mins        ░░░░░░░░░░░░░░░░░░░░   0.8%',
-    'GraphQL     12 mins        ░░░░░░░░░░░░░░░░░░░░   0.7%',
-    'TOML        9 mins         ░░░░░░░░░░░░░░░░░░░░░   0.6%',
-    'INI         1 min          ░░░░░░░░░░░░░░░░░░░░░   0.1%',
-    'XML         1 min          ░░░░░░░░░░░░░░░░░░░░░   0.1%',
-    'Bash        0 secs         ░░░░░░░░░░░░░░░░░░░░░   0.0%',
-    'CSS         0 secs         ░░░░░░░░░░░░░░░░░░░░░   0.0%',
-    'HTML        0 secs         ░░░░░░░░░░░░░░░░░░░░░   0.0%',
-    'Text        0 secs         ░░░░░░░░░░░░░░░░░░░░░   0.0%',
-])
+# ファイルコンテンツを生成します。
+# response の内容は https://wakatime.com/developers/#stats
+languages_raw_data = response_json['data']['languages']
+file_content = '\n'.join((generate_file_content_line(_)
+                          for _ in languages_raw_data))
+logger.info('gist 更新内容生成完了。')
 
 # 認証は access token で行います。
 headers = {
@@ -108,12 +143,12 @@ headers = {
 }
 # gist を更新します。
 data = json.dumps({
-    # NOTE: なくても良い。
+    # NOTE: description を更新したいときは有効化。
     # 'description': 'test',
     'files': {
         # 更新ファイル名。
         'file': {
-            'content': temporary_file_content,
+            'content': file_content,
         }
     },
 })
@@ -121,5 +156,7 @@ response = requests.post(
     f'https://api.github.com/gists/{get_env("GIST_ID")}',
     headers=headers,
     data=data)
+logger.info(f'gist 更新完了。ステータスコード:{response.status_code}')
 response_json = json.loads(response.text)
-pprint(response_json)
+logger.info(f'更新内容。\n{response_json["files"]["file"]["content"]}')
+logger.info(f'処理終了。')
